9938f5984137a6772461f4e0523ea558423acf15
0luv69 <baniyarujal@gmail.com>
2024-06-07 20:14:31 +0545

    update

--- print_record/main_app/views.py
+++ print_record/main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
-from .serializer import *
+from .serializers import *
 # Create your views here.
 
 class stdApi(APIView):
@@ -45,4 +45,15 @@ class stdApi(APIView):
 
                 
 
+class StaffApi(APIView):
+    authentication_classes = []
+    permission_classes = []
 
+    
+    def post(self, request):
+        serializer = StaffSerializer(data=request.data)
+
+        if serializer.is_valid():
+            hospital = serializer.save()
+            return Response({'data': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

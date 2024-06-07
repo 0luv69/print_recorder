5b192ef410bfcf74fbebcf39633975aca823c83b
0luv69 <baniyarujal@gmail.com>
2024-06-07 19:34:40 +0545

    added patch freatures too

--- print_record/main_app/views.py
+++ print_record/main_app/views.py
@@ -5,29 +5,44 @@ from .models import *
 from .serializer import *
 # Create your views here.
 
-
-
 class stdApi(APIView):
     def get(self, request):
         std_all_obj = Student.objects.all()
-        std_ser_var = StdSerializer(std_all_obj, many=True)
+        ser_std_var = StdSerializer(std_all_obj, many=True)
 
         return Response({
-            'payload' : std_ser_var.data,
+            'payload' : ser_std_var.data,
             'Message': 'Success Fetched'
         },
         status=200)
     
     def post(self, request):
         data = request.data 
-        std_ser_var = StdSerializer(data=data)
+        ser_std_var = StdSerializer(data=data)
 
-        if std_ser_var.is_valid():
-            std_ser_var.save()
+        if ser_std_var.is_valid():
+            ser_std_var.save()
             return Response({
-                    'payload' : std_ser_var.data,
+                    'payload' : ser_std_var.data,
                     'Message': 'Success Saved'
             }, status=200
             )
-        return Response({ 'payload': std_ser_var.data, 'error': std_ser_var.errors }, status=403)
+        return Response({ 'payload': ser_std_var.data, 'error': ser_std_var.errors }, status=403)
+
+    # # ?id=1
+    def patch(self, request):
+        id = request.GET.get('id')
+        data = request.data 
+        try:
+            std_obj= Student.objects.get(id=id)
+            ser_std_var= StdSerializer(std_obj, data=data, partial= True)
+            if ser_std_var.is_valid():
+                ser_std_var.save()
+                return Response({'payload': data,'message': 'patch student success' }, status=200)
+            return Response({'payload': data,'error reason' : ser_std_var.errors , 'message': 'Bad request here' }, status= 403,)
+        except Exception as e:
+            return Response({'error reason' : f'{e}' , 'message': 'hint: ID is not valid'}, status= 403)
+
+                
+
 
